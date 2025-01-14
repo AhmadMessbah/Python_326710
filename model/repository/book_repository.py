ddf3618134eba416c027c9f761c51b6988494e4f
Ashkan Jafarinejad <ashkanaj2019@gmail.com>
2025-01-14 14:00:18 +0330

    Create README.md

---
+++ model/repository/book_repository.py
@@ -0,0 +1,13 @@
+class BookRepository:
+    def save(self, book):
+        print(f"Saved In Database: {book}")
+    def edit(self):
+        pass
+    def delete(self):
+        pass
+    def find_all(self):
+        pass
+    def find_by_id(self, id):
+        pass
+    def find_by_name(self, name):
+        pass
--- test/book_test.py
+++ test/book_test.py
@@ -1,4 +1,10 @@
 from model.entity.book import Book
+from model.repository.book_repository import BookRepository
+
 #Entity test
 book1=Book("Harry Potter","J. K. Rowling",233)
 print(book1)
+
+
+book_repo = BookRepository()
+book_repo.save(book1)
