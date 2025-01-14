c2e25b696fd98e90adb300580b4dc8af952f9732
Messbah.A <messbah.a@gmail.com>
2025-01-14 13:40:02 +0330

    Create README.md

--- model/entity/book.py
+++ model/entity/book.py
@@ -2,3 +2,28 @@
 # name str
 # write str
 # pages int
+import re
+from tkinter.font import names
+
+
+class Book:
+    def __init__(self, name, write, pages):
+        self.id = None
+        self.name = name
+        self.write = write
+        self.pages = pages
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if re.match(r"^[a-zA-Z\s]{2,30}$", value):
+            self._name = value
+
+        else :
+            raise ValueError("Name can only contain alphanumeric characters")
+
+    def __repr__(self):
+        return f"Name : {self.name},Writer :{self.write},Pages :{self.pages}"
