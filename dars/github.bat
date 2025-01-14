29201461ef9045f7b007fea7930f354b7838d67a
Messbah.A <messbah.a@gmail.com>
2025-01-14 13:40:02 +0330

    Create README.md

--- dars/github.bat
+++ dars/github.bat
@@ -1,2 +1,4 @@
 git config --global user.email "messbah.a@gmail.com"
 git config --global user.name "Messbah.A"
+git config --global user.email "ashkanaj2019@gmail.com"
+git config --global user.name "ashkanaj"
---
+++ test/book_test.py
@@ -0,0 +1,4 @@
+from model.entity.book import Book
+#Entity test
+book1=Book("Harry Potter","J. K. Rowling",233)
+print(book1)
